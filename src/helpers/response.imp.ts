9a5c7c97c39c7333762ee574fe0cbac40a2e6e81
Esclender <elesclenderlugo@gmail.com>
2024-03-25 13:19:42 -0500

    feat: no info && exitoso in json response

--- src/helpers/response.imp.ts
+++ src/helpers/response.imp.ts
@@ -1,14 +1,9 @@
 import { responseDto } from '../domain/dtos'
 
 export default function ResponseImplementation( dto: responseDto ) {
-  const exitoso = dto.status >= 200 && dto.status < 300 
-    ? true
-    : false
+  
   
   return dto.res
     .status( dto.status )
-    .json( {
-      exitoso,
-      info: dto.data
-    } )
+    .json( dto.data )
 }
